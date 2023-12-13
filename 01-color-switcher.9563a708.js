ed98d954a6ef79ddfb73d12abd9313faedc262da
vladvolchek <137838076+vladvolchek@users.noreply.github.com>
2023-12-13 20:04:54 +0000

    Deploying to gh-pages from @ vladvolchek/goit-js-hw-09@a646eac5528ce883e846465404d37cabb374580a 🚀

---
+++ 01-color-switcher.9563a708.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;const d=()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`};t.addEventListener("click",(()=>{a=setInterval(d,1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.9563a708.js.map
